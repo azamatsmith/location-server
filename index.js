8eb8ff5936a39bf15783c2cdda48a308381b1195
Matthew Smith <azamatsmith@gmail.com>
2018-05-03 11:10:08 -0600

    Revert "Revert "Initial commit""
    This reverts commit 303a10abe9f3bf837ae89f2ac90bcc0983cf0a08.

---
+++ index.js
@@ -0,0 +1,50 @@
+require('dotenv').load();
+const {MongoClient} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 6001;
+
+// Connect to mongo db
+let dbo;
+
+MongoClient.connect(process.env.MONGO_URI, (err, client) => {
+  if (err) throw err;
+  console.log('db ready');
+  dbo = client.db('map-tut');
+});
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  console.log('request to /');
+  res.send('it works');
+});
+
+app.post('/get-markers', (req, res) => {
+  const collection = dbo.collection('locations');
+  // collection.find({
+  //   geo: {
+  //     $geoWithin: {
+  //       $geometry: {
+  //         type: 'Polygon',
+  //         coordinates: [[[0, 0], [3, 6], [6, 1], [0, 0]]],
+  //       },
+  //     },
+  //   },
+  // });
+  const objs = collection.find({}).toArray((err, result) => {
+    if (err) {
+      return res.json({error: 'could not get locations'});
+    }
+    res.json(result);
+  });
+});
+
+app.listen(port, () => {
+  console.log('app listening on port ', port);
+});
